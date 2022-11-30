56c2a67413eb25a67047b08cafb7c1d619ab6fce
Kopatych1 <ugvkf41@gmail.com>
2022-11-30 15:09:41 +0500

    2

--- app/src/main/java/com/example/myapplication/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -18,32 +18,7 @@ import java.io.Serializable;
 
 
 public class Profile extends AppCompatActivity {
-    private String name;
-    private String specie;
-    private int age;
-    public Profile(String name,String specie,int age){
-        this.name = name;
-        this.specie = specie;
-        this.age = age;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public int getAge(){
-        return age;
-    }
-    public void setAge(int age){
-        this.age = age;
-    }
-    public String getSpecie(){
-        return specie;
-    }
-    public void setSpecie(String specie){
-        this.specie = specie;
-    }
+
     private ImageView imageView;
     private final int Pick_image = 1;
     @Override
---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+public class User {
+    private String name;
+    private String specie;
+    private int age;
+    public User(String name,String specie,int age){
+        this.name = name;
+        this.specie = specie;
+        this.age = age;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public String getSpecie(){
+        return specie;
+    }
+    public void setSpecie(String specie){
+        this.specie = specie;
+    }
+}
